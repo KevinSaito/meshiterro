955a711b1e2c87d0db2e3ea3da9082ce3d33d600
EC2 Default User <u6cfdm3p@gmail.com>
2022-01-13 13:16:27 +0000

    [Update]11章終了

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -8,14 +8,15 @@ class PostImagesController < ApplicationController
     @post_image.user_id = current_user.id
     @post_image.save
     redirect_to post_images_path
-  end 
-  
-  def index
   end
-  
+
+  def index
+    @post_images = PostImage.all
+  end
+
   def show
   end
-  
+
   private
   def post_image_params
     params.require(:post_image).permit(:shop_name,:image,:caption)
