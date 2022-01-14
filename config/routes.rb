c62279b1263741e57ef78b4b42eebcfd66914294
EC2 Default User <u6cfdm3p@gmail.com>
2022-01-14 07:51:51 +0000

    [Update]ユーザー詳細ページ作成中

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  get 'users/show'
+  get 'users/edit'
   root to: "homes#top"
   devise_for :users
 
-  resources :post_images, only:[:new,:create,:index,:show]
+  resources :post_images, only:[:new,:create,:index,:show,:destroy]
+  
+  resources :users, only:[:show,:edit]
 
 
 
